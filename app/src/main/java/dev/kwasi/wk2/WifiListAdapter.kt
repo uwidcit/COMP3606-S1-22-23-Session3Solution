2c6dc15a4c25c9aaa2abba7c67332dfc2752d566
shanif <97924668+salamandur@users.noreply.github.com>
2023-09-14 10:45:31 -0400

    Update WifiListAdapter.kt

--- app/src/main/java/dev/kwasi/wk2/WifiListAdapter.kt
+++ app/src/main/java/dev/kwasi/wk2/WifiListAdapter.kt
@@ -5,9 +5,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import android.util.log
 
 class WifiListAdapter(private val wifiNetworks: List<WifiNetwork>) :
-    RecyclerView.Adapter<WifiListAdapter.ViewHolder>() {
+    RecyclerView.Adapter<WifiListAdapter.ViewHolder>(), View.OnClickListener {
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val ssidTextView: TextView = itemView.findViewById(R.id.ssidTextView)
@@ -16,6 +17,7 @@ class WifiListAdapter(private val wifiNetworks: List<WifiNetwork>) :
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_wifi, parent, false)
+        view.setOnClickListener(this)
         return ViewHolder(view)
     }
 
@@ -27,5 +29,11 @@ class WifiListAdapter(private val wifiNetworks: List<WifiNetwork>) :
 
     override fun getItemCount(): Int {
         return wifiNetworks.size
+    }
+    override fun onClick(v: View?) {
+        if(v != null){
+            val ssid = v.findViewById<TextView>(R.id.ssidTextView)
+        }
+        Log.e(tag:"CLICK", msg:"I clicked the view: SSID = " + ssid.text)
     }
 }
